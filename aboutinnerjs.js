c3d191bc5e874151e77284db1cf89be2dc133b74
sailajauppalapati <sailajauppalapati99@gmail.com>
2020-09-01 17:35:10 +0530

    javascript added.

---
+++ aboutinnerjs.js
@@ -0,0 +1,4 @@
+function printthis(){
+	document.write("JavaScript is a programming language that adds interactivity to your website. This happens in games, in the behavior of responses when buttons are pressed or with data entry on forms; with dynamic styling; with animation, etc..JavaScript is versatile and beginner-friendly. With more experience, you'll be able to create games, animated 2D and 3D graphics, comprehensive database-driven apps, and much more!");
+}
+
